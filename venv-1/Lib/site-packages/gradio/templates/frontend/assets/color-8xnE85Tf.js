24ae2df7e306b5b6fb8444433cf359aba8e3b31c
arpitbytes507 <arpit.dhumane@gmail.com>
2025-06-20 00:13:47 +0530

    Add notebook and dataset only

--- venv-1/Lib/site-packages/gradio/templates/frontend/assets/color-8xnE85Tf.js
+++
@@ -1,2 +0,0 @@
-import{o}from"./index-BegVNm_t.js";const t=r=>o[r%o.length];export{t as g};
-//# sourceMappingURL=color-8xnE85Tf.js.map
--- venv-1/Lib/site-packages/gradio/templates/node/build/client/_app/immutable/chunks/pie-BEWT4RHE.D9m_7Kze.js
+++
@@ -1,5 +0,0 @@
-import { b, d } from "./mermaid-parser.core.CYXMBtqd.js";
-export {
-  b as PieModule,
-  d as createPieServices
-};
